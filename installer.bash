47dcc2ac9dddf1e26bb019c12dd92d0bce8c0603
RyodoTanaka <k104073r@mail.kyutech.jp>
2015-09-12 20:15:31 +0900

    add PCL installer
    PCLをインストールしてくれるスクリプトを作成。

---
+++ installer.bash
@@ -0,0 +1,39 @@
+#!/bin/bash
+
+pwd_dir=$(pwd)
+
+if [ "$1" = "--auto" ]; then
+	auto_flg=1
+else
+	auto_flg=0
+fi
+
+echo "
+                    ██████╗  ██████╗██╗                               
+                    ██╔══██╗██╔════╝██║                               
+                    ██████╔╝██║     ██║                               
+                    ██╔═══╝ ██║     ██║                               
+                    ██║     ╚██████╗███████╗                          
+                    ╚═╝      ╚═════╝╚══════╝                          
+                                                                      
+██╗███╗   ██╗███████╗████████╗ █████╗ ██╗     ██╗     ███████╗██████╗ 
+██║████╗  ██║██╔════╝╚══██╔══╝██╔══██╗██║     ██║     ██╔════╝██╔══██╗
+██║██╔██╗ ██║███████╗   ██║   ███████║██║     ██║     █████╗  ██████╔╝
+██║██║╚██╗██║╚════██║   ██║   ██╔══██║██║     ██║     ██╔══╝  ██╔══██╗
+██║██║ ╚████║███████║   ██║   ██║  ██║███████╗███████╗███████╗██║  ██║
+╚═╝╚═╝  ╚═══╝╚══════╝   ╚═╝   ╚═╝  ╚═╝╚══════╝╚══════╝╚══════╝╚═╝  ╚═╝
+
+
+                                                                      "
+if [ ${auto_flg} -eq 0 ]; then
+	printf "Ready to Install. Start ? (yes/no) : "
+	read -r start_flg
+	if [ ${start_flg} != "y" -a ${start_flg} != "yes" ]; then
+		echo "OK. Stop to install."
+		cd ${pwd_dir}
+		return 0
+	fi
+fi
+yes | sudo add-apt-repository ppa:v-launchpad-jochen-sprickerhof-de/pcl
+sudo apt-get update
+sudo apt-get install -yV libpcl-all
