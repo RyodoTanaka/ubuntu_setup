b81673b640860269ec4e5435c47e6369f5dfb4b2
RyodoTanaka <groadpg@gmail.com>
2019-10-08 20:14:14 +0900

    Update emacs installer

--- installer.bash
+++ installer.bash
@@ -1,13 +1,4 @@
 #!/bin/bash
-
-pwd_dir=$(pwd)
-
-if [ "$1" = "--auto" ]; then
-	auto_flg=1
-else
-	auto_flg=0
-fi
-
 echo "
   ______                                 
  |  ____|                                
@@ -22,19 +13,52 @@ echo "
  |_____|_| |_|___/\__\__,_|_|_|\___|_|   
                                          
 "
-  
+
+script_path=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
+pwd_path=$(pwd)
+
+version = 26.1 
+
+# Check installation version
+if [ -e $1 ]; then
+	echo "Install : Emacs "${version}" (Default)"
+else
+	echo "Install : Emacs "$1
+	version=$1
+fi
+
+# Check auto install
+if [ "$2" = "--auto" ]; then
+	auto_flg=1
+else
+	auto_flg=0
+fi
+
 if [ ${auto_flg} -eq 0 ]; then
-    printf "Ready to Install. Start ? (yes/no) : "
+    printf "Ready to Install. ? (yes/no) : "
     read -r start_flg
     if [ ${start_flg} != "y" -a ${start_flg} != "yes" ]; then
 	echo "OK. Stop to install."
-	cd ${pwd_dir}
 	return 0
     fi
 fi
 
-sudo apt -yV install emacs24
-sudo apt -yV install emacs-mozc emacs-mozc-bin
+# install emacs${version}
+# install dependencies
+sudo apt -yV install libgnutls28-dev
+sudo apt -yV install libxpm-dev
+
+
+cd ${script_path}/source
+wget https://ftp.gnu.org/pub/gnu/emacs/emacs-${version}.tar.gz
+tar -xvzf emacs-${version}.tar.gz
+cd ${script_path}/source/emacs-${version}
+./configure
+make -j`nproc`
+sudo make -j`nproc` install 
+cd ${pwd_path}
+
+# set ~/.emacs.d
 cd ~
-git clone https://github.com/RyodoTanaka/.emacs.d.git
-cd ${pwd_dir}
+git clone -b master https://github.com/RyodoTanaka/.emacs.d.git
+cd ${pwd_path}
